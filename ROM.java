87b38b8e575e02dbba4dc754f11cd041703fd14e
Jay Morrow <Takuya813@gmail.com>
2009-09-01 19:50:08 +0000

    Added header checksumming

--- ROM.java
+++ ROM.java
@@ -109,6 +109,43 @@ public class ROM{
 		}
 		System.out.println("RAM Size: "+out);
 	}
+
+	//Verify checksum of ROM
+	public void verifyChecksum(){
+		final int[] nintyBitmap = 
+		{0xCE,0xED,0x66,0x66,0xCC,0x0D,0x00,0x0B,0x03,
+		 0x73,0x00,0x83,0x00,0x0C,0x00,0x0D,0x00,0x08,
+		 0x11,0x1F,0x88,0x89,0x00,0x0E,0xDC,0xCC,0x6E,
+		 0xE6,0xDD,0xDD,0xD9,0x99,0xBB,0xBB,0x67,0x63,
+		 0x6E,0x0E,0xEC,0xCC,0xDD,0xDC,0x99,0x9F,0xBB, 
+		 0xB9,0x33,0x3E};
+	  
+		int ptr=0x0104;
+		int x=0;
+		
+		while (ptr <= 0x0133){
+			if(MEM[ptr]!=nintyBitmap[ptr-0x0104])
+				{
+					System.out.println("Bitmap Invalid");
+					System.out.println("Header Checksum Invalid");
+					break;
+				}
+		ptr++;
+		}
+		if(ptr==0x0134)
+		{
+			//x=0:FOR i=0134h TO 014Ch:x=x-MEM[i]-1:NEXT
+	  		for(;ptr<=0x014C;ptr++)
+			{
+				x=x-MEM[ptr]-1; //checksum algorithm
+			}
+			if((x&0xFF) == MEM[ptr])
+				System.out.println("Header Checksum Valid");
+			else
+				System.out.println("Header Checksum Invalid");
+		}
+	}
+	
 	// Prints game title
 	public void printTitle(){
 		System.out.print("ROM Title: ");
@@ -119,6 +156,4 @@ public class ROM{
 		}
 		System.out.println();
 	}
-	
-	
 }
